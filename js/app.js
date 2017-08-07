29c87ce7bfbd2f6b353b3617abe6db537cfaed13
Federico Cano <canofedericomartin@gmail.com>
2017-08-07 10:30:09 -0300

    Add static EditableTimerList component

--- js/app.js
+++ js/app.js
@@ -11,3 +11,25 @@ class TimersDashboard extends React.Component {
   }
 }
 
+class EditableTimerList extends React.Component {
+  render () {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web domination'
+          elapse='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen={true}
+        />
+      </div>
+    );
+  }
+}
