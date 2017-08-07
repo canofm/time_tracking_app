0ba98472c65a21ce234cca2c6eb3d8df46747c47
Federico Cano <canofedericomartin@gmail.com>
2017-08-07 10:29:42 -0300

    Add static TimersDashboard component

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+class TimersDashboard extends React.Component {
+  render() {
+    return (
+      <div className='ui tree column centered grid'>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm isOpen={true} />
+        </div>
+      </div>
+    );
+  }
+}
 
